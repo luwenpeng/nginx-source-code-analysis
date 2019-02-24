c51a52a015265b2f6727adc2b670779abcc25800
lwp <18203687190@163.com>
2019-02-24 17:26:29 +0800

    [add] (os) ngx_daemon.c

--- nginx-1.12.2/src/os/unix/ngx_daemon.c
+++ nginx-1.12.2/src/os/unix/ngx_daemon.c
@@ -9,23 +9,31 @@
 #include <ngx_core.h>
 
 
+/*
+ * brief  : 使调用进程变为守护进程.
+ * return : NGX_ERROR/NGX_OK
+ */
 ngx_int_t
 ngx_daemon(ngx_log_t *log)
 {
     int  fd;
 
     switch (fork()) {
+    // fork 失败
     case -1:
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "fork() failed");
         return NGX_ERROR;
 
+    // 子进程
     case 0:
         break;
 
+    // 父进程
     default:
         exit(0);
     }
 
+    // 获取 fork 后的进程 id
     ngx_pid = ngx_getpid();
 
     if (setsid() == -1) {
@@ -35,6 +43,7 @@ ngx_daemon(ngx_log_t *log)
 
     umask(0);
 
+    // 以读写模式打开 /dev/null
     fd = open("/dev/null", O_RDWR);
     if (fd == -1) {
         ngx_log_error(NGX_LOG_EMERG, log, ngx_errno,
@@ -59,6 +68,7 @@ ngx_daemon(ngx_log_t *log)
     }
 #endif
 
+    // 关闭 /dev/null 的文件句柄
     if (fd > STDERR_FILENO) {
         if (close(fd) == -1) {
             ngx_log_error(NGX_LOG_EMERG, log, ngx_errno, "close() failed");
