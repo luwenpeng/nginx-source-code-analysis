39d919615dd58fd09844fa1250c12762a57a8889
lwp <18203687190@163.com>
2019-02-24 17:22:57 +0800

    [add] (event) ngx_event_posted.c

--- nginx-1.12.2/src/event/ngx_event_posted.c
+++ nginx-1.12.2/src/event/ngx_event_posted.c
@@ -14,6 +14,9 @@ ngx_queue_t  ngx_posted_accept_events;
 ngx_queue_t  ngx_posted_events;
 
 
+/*
+ * brief  : 处理 posted 队列中的所有事件, 优先处理队首的事件.
+ */
 void
 ngx_event_process_posted(ngx_cycle_t *cycle, ngx_queue_t *posted)
 {
@@ -22,14 +25,19 @@ ngx_event_process_posted(ngx_cycle_t *cycle, ngx_queue_t *posted)
 
     while (!ngx_queue_empty(posted)) {
 
+        // 取第一个结点
         q = ngx_queue_head(posted);
+
+        // 获取结点 q 中的数据
         ev = ngx_queue_data(q, ngx_event_t, queue);
 
         ngx_log_debug1(NGX_LOG_DEBUG_EVENT, cycle->log, 0,
                       "posted event %p", ev);
 
+        // 将 ev 从其所在的事件队列中删除
         ngx_delete_posted_event(ev);
 
+        // 使用 ev 注册的 handler 处理 ev 事件
         ev->handler(ev);
     }
 }
