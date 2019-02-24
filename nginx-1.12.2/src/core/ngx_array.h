f942528174ced683052b9de7709af48ef8f64156
lwp <18203687190@163.com>
2019-02-24 17:11:04 +0800

    [add] (core) ngx_array.h

--- nginx-1.12.2/src/core/ngx_array.h
+++ nginx-1.12.2/src/core/ngx_array.h
@@ -14,11 +14,11 @@
 
 
 typedef struct {
-    void        *elts;
-    ngx_uint_t   nelts;
-    size_t       size;
-    ngx_uint_t   nalloc;
-    ngx_pool_t  *pool;
+    void        *elts;   // 该数组中基本存储单元数组的起始地址
+    ngx_uint_t   nelts;  // 该数组中已经使用的基本存储单元的个数
+    size_t       size;   // 该数组中基本存储单元的大小
+    ngx_uint_t   nalloc; // 该数组中基本存储单元的个数
+    ngx_pool_t  *pool;   // 该数组用于分配内存的内存池指针
 } ngx_array_t;
 
 
@@ -28,6 +28,14 @@ void *ngx_array_push(ngx_array_t *a);
 void *ngx_array_push_n(ngx_array_t *a, ngx_uint_t n);
 
 
+/*
+ * brief  : 初始化由 n 个大小为 size 的基本存储单元组成的数组.
+ * param  : [in] arry 结构体数组的管理结构指针
+ * param  : [in] pool 该数组用于分配内存的内存池指针
+ * param  : [in] n 该数组中基本存储单元的个数
+ * param  : [in] size 该数组中基本存储单元的大小
+ * return : NGX_ERROR/NGX_OK
+ */
 static ngx_inline ngx_int_t
 ngx_array_init(ngx_array_t *array, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
