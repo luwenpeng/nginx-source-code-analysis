7cefe3ee57e4e952440333722c16064d909d1bb8
lwp <18203687190@163.com>
2019-02-24 17:20:49 +0800

    [add] (core) ngx_shmtx.c

--- nginx-1.12.2/src/core/ngx_shmtx.c
+++ nginx-1.12.2/src/core/ngx_shmtx.c
@@ -11,10 +11,15 @@
 
 #if (NGX_HAVE_ATOMIC_OPS)
 
+/************************** 若支持原子锁则使用原子锁 **************************/
 
 static void ngx_shmtx_wakeup(ngx_shmtx_t *mtx);
 
 
+/*
+ * brief  : 创建用于共享内存的信号量．
+ * return : NGX_OK
+ */
 ngx_int_t
 ngx_shmtx_create(ngx_shmtx_t *mtx, ngx_shmtx_sh_t *addr, u_char *name)
 {
@@ -24,12 +29,18 @@ ngx_shmtx_create(ngx_shmtx_t *mtx, ngx_shmtx_sh_t *addr, u_char *name)
         return NGX_OK;
     }
 
+    // 设置 spin 上限
     mtx->spin = 2048;
 
 #if (NGX_HAVE_POSIX_SEM)
 
     mtx->wait = &addr->wait;
 
+    /*
+     * 初始化 mtx->sem 指针指向的信号量, 并设置初始值为 0.
+     * 参数 ‘1’ 表示信号量在进程之间共享, 并且应位于共享内存的区域中.
+     * fork(2) 创建的子进程继承父进程的内存映射, 可以访问信号量.
+     */
     if (sem_init(&mtx->sem, 1, 0) == -1) {
         ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, ngx_errno,
                       "sem_init() failed");
@@ -43,6 +54,9 @@ ngx_shmtx_create(ngx_shmtx_t *mtx, ngx_shmtx_sh_t *addr, u_char *name)
 }
 
 
+/*
+ * brief  : 销毁用于共享内存的信号量．
+ */
 void
 ngx_shmtx_destroy(ngx_shmtx_t *mtx)
 {
@@ -59,6 +73,11 @@ ngx_shmtx_destroy(ngx_shmtx_t *mtx)
 }
 
 
+/*
+ * brief  : 尝试获取共享内存的原子锁 mtx->lock, 并将其值设置为 ngx_pid.
+ * return : 1 : 成功
+ *          0 : 失败
+ */
 ngx_uint_t
 ngx_shmtx_trylock(ngx_shmtx_t *mtx)
 {
@@ -66,21 +85,28 @@ ngx_shmtx_trylock(ngx_shmtx_t *mtx)
 }
 
 
+/*
+ * brief  : 一定会获取到锁, 获取不到不返回(忙等待).
+ */
 void
 ngx_shmtx_lock(ngx_shmtx_t *mtx)
 {
-    ngx_uint_t         i, n;
+    ngx_uint_t  i, n;
 
     ngx_log_debug0(NGX_LOG_DEBUG_CORE, ngx_cycle->log, 0, "shmtx lock");
 
+    // 以获取自旋锁的形式获取锁
     for ( ;; ) {
 
+        // 成功获取到锁
         if (*mtx->lock == 0 && ngx_atomic_cmp_set(mtx->lock, 0, ngx_pid)) {
             return;
         }
 
+        // 未获取到锁，则不断尝试
         if (ngx_ncpu > 1) {
 
+            // 自旋锁循环次数从 1,2,4,8,..., 到 spin
             for (n = 1; n < mtx->spin; n <<= 1) {
 
                 for (i = 0; i < n; i++) {
@@ -95,11 +121,14 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
             }
         }
 
+        // 长时间未获得到锁
 #if (NGX_HAVE_POSIX_SEM)
 
         if (mtx->semaphore) {
+            // wait 原子加 1, 表示有几个进程在此 sem 上面 sem_wait
             (void) ngx_atomic_fetch_add(mtx->wait, 1);
 
+            // 获取到锁, mtx->wait 减 1
             if (*mtx->lock == 0 && ngx_atomic_cmp_set(mtx->lock, 0, ngx_pid)) {
                 (void) ngx_atomic_fetch_add(mtx->wait, -1);
                 return;
@@ -133,6 +162,9 @@ ngx_shmtx_lock(ngx_shmtx_t *mtx)
 }
 
 
+/*
+ * brief  : 解锁
+ */
 void
 ngx_shmtx_unlock(ngx_shmtx_t *mtx)
 {
@@ -146,6 +178,11 @@ ngx_shmtx_unlock(ngx_shmtx_t *mtx)
 }
 
 
+/*
+ * brief  : 强制解锁, 并唤醒其他等待信号量的进程．
+ * return : 1 : 成功
+ *          0 : 失败
+ */
 ngx_uint_t
 ngx_shmtx_force_unlock(ngx_shmtx_t *mtx, ngx_pid_t pid)
 {
@@ -161,6 +198,9 @@ ngx_shmtx_force_unlock(ngx_shmtx_t *mtx, ngx_pid_t pid)
 }
 
 
+/*
+ * brief  : 唤醒 ngx_shmtx_lock() 忙等待阶段的进程．
+ */
 static void
 ngx_shmtx_wakeup(ngx_shmtx_t *mtx)
 {
@@ -197,6 +237,8 @@ ngx_shmtx_wakeup(ngx_shmtx_t *mtx)
 
 
 #else
+
+/************************* 若不支持原子锁则使用文件锁 *************************/
 
 
 ngx_int_t
