b6a85fabdfcf7c9f77de754689905bec1f37f149
lwp <18203687190@163.com>
2019-02-24 17:17:51 +0800

    [add] (core) ngx_times.h

--- nginx-1.12.2/src/core/ngx_times.h
+++ nginx-1.12.2/src/core/ngx_times.h
@@ -14,9 +14,9 @@
 
 
 typedef struct {
-    time_t      sec;
-    ngx_uint_t  msec;
-    ngx_int_t   gmtoff;
+    time_t      sec; // 秒
+    ngx_uint_t  msec; // 毫秒
+    ngx_int_t   gmtoff; // GMT 偏移量
 } ngx_time_t;
 
 
@@ -33,7 +33,9 @@ time_t ngx_next_time(time_t when);
 
 extern volatile ngx_time_t  *ngx_cached_time;
 
+// 从缓存中获取从格林威治时间1970年1月1日0点0分0秒到当前时间的秒数
 #define ngx_time()           ngx_cached_time->sec
+// 获取时间缓存结构体
 #define ngx_timeofday()      (ngx_time_t *) ngx_cached_time
 
 extern volatile ngx_str_t    ngx_cached_err_log_time;
