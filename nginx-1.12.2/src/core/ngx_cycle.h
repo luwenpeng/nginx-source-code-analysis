fb542304555586b5b8d055249e965f5bd4bfccbb
lwp <18203687190@163.com>
2019-04-27 11:13:18 +0800

    [add] (core) ngx_cycle.h

--- nginx-1.12.2/src/core/ngx_cycle.h
+++ nginx-1.12.2/src/core/ngx_cycle.h
@@ -36,49 +36,81 @@ struct ngx_shm_zone_s {
 
 
 struct ngx_cycle_s {
+    // 存储所有模块 create_conf() 返回值的指针数组
     void                  ****conf_ctx;
+    // 当前 cycle 所使用的内存池
     ngx_pool_t               *pool;
 
+    // 初始化时存储从 old_cycle->log 传递过来的日志对象，初始化后存储 cycle->new_log
     ngx_log_t                *log;
+    // cycle 自己创建的日志对象
     ngx_log_t                 new_log;
 
+    // 将错误输出到 stderr 的标示
     ngx_uint_t                log_use_stderr;  /* unsigned  log_use_stderr:1; */
 
+    // 元素类型为 ngx_connection_t *，元素个数为 cycle->files_n 的指针数组
     ngx_connection_t        **files;
+    // 依靠 next * 指针连接起来的空闲连接的链表
     ngx_connection_t         *free_connections;
+    // 空闲连接的个数
     ngx_uint_t                free_connection_n;
 
+    // 存储所有模块地址 (ngx_module_t *) 的指针数组
     ngx_module_t            **modules;
+    // 模块的数量
     ngx_uint_t                modules_n;
+    // 防止加载额外的模块的标示，在执行 ngx_count_modules() 后不可再加载动态模块
     ngx_uint_t                modules_used;    /* unsigned  modules_used:1; */
 
+    // 可重用的连接的队列
     ngx_queue_t               reusable_connections_queue;
+    // 可重用的连接的个数
     ngx_uint_t                reusable_connections_n;
 
+    // 存储监听端口 (ngx_listening_t) 信息的数组
     ngx_array_t               listening;
+    // 存储路径指针 (ngx_path_t *) 信息的数组
     ngx_array_t               paths;
 
+    // 储存 (ngx_conf_dump_t) 信息的数组
     ngx_array_t               config_dump;
+    // config_dump 红黑树的根节点
     ngx_rbtree_t              config_dump_rbtree;
+    // config_dump 红黑树的哨兵结点
     ngx_rbtree_node_t         config_dump_sentinel;
 
+    // 存储打开文件 (ngx_open_file_t) 信息的链表
     ngx_list_t                open_files;
+    // 存储共享内存 (ngx_shm_zone_t) 信息的链表
     ngx_list_t                shared_memory;
 
+    // 单个进程所支持的最大连接数，对应于 worker_connections 配置项
     ngx_uint_t                connection_n;
+    // 进程最大打开文件数
     ngx_uint_t                files_n;
 
+    // 元素类型为 ngx_connection_t，元素个数为 cycle->connection_n 的连接数组
     ngx_connection_t         *connections;
+    // 元素类型为 ngx_event_t，元素个数为 cycle->connection_n 可读事件数组
     ngx_event_t              *read_events;
+    // 元素类型为 ngx_event_t，元素个数为 cycle->connection_n 可写事件数组
     ngx_event_t              *write_events;
 
+    // 老的 cycle
     ngx_cycle_t              *old_cycle;
 
+    // 配置文件路径
     ngx_str_t                 conf_file;
+    // 配置参数，例如 ./nginx -g xxx 中的 xxx
     ngx_str_t                 conf_param;
+    // 配置项部署路径
     ngx_str_t                 conf_prefix;
+    // nginx 安装路径
     ngx_str_t                 prefix;
+    // 当系统不支持原子锁时，使用文件锁，此变量存储 lock_file 的路径
     ngx_str_t                 lock_file;
+    // 以小写字母存储的主机名
     ngx_str_t                 hostname;
 };
 
