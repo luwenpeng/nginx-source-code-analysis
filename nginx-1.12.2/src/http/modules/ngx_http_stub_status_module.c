dc5f5edb0786611215c44477d9a4b4049740305e
lwp <18203687190@163.com>
2019-04-27 11:16:58 +0800

    [add] (modules) ngx_http_stub_status_module.c

--- nginx-1.12.2/src/http/modules/ngx_http_stub_status_module.c
+++ nginx-1.12.2/src/http/modules/ngx_http_stub_status_module.c
@@ -10,6 +10,99 @@
 #include <ngx_http.h>
 
 
+/*
+***************************************************
+* 定义 event/ngx_event.c                          *
+***************************************************
+
+    #if (NGX_STAT_STUB)
+
+    static ngx_atomic_t   ngx_stat_accepted0;
+    ngx_atomic_t         *ngx_stat_accepted = &ngx_stat_accepted0;
+    static ngx_atomic_t   ngx_stat_handled0;
+    ngx_atomic_t         *ngx_stat_handled  = &ngx_stat_handled0;
+    static ngx_atomic_t   ngx_stat_requests0;
+    ngx_atomic_t         *ngx_stat_requests = &ngx_stat_requests0;
+    static ngx_atomic_t   ngx_stat_active0;
+    ngx_atomic_t         *ngx_stat_active   = &ngx_stat_active0;
+    static ngx_atomic_t   ngx_stat_reading0;
+    ngx_atomic_t         *ngx_stat_reading  = &ngx_stat_reading0;
+    static ngx_atomic_t   ngx_stat_writing0;
+    ngx_atomic_t         *ngx_stat_writing  = &ngx_stat_writing0;
+    static ngx_atomic_t   ngx_stat_waiting0;
+    ngx_atomic_t         *ngx_stat_waiting  = &ngx_stat_waiting0;
+
+    #endif
+
+***************************************************
+* 初始化 ngx_event_module_init()                  *
+***************************************************
+
+ngx_stat_accepted, ngx_stat_handled, ngx_stat_requests,
+ngx_stat_active,   ngx_stat_reading, ngx_stat_writing,
+ngx_stat_waiting 等指针指向共享内存中创建的 ngx_atomic_t 变量。
+
+    #if (NGX_STAT_STUB)
+
+    ngx_stat_accepted = (ngx_atomic_t *) (shared + 3 * cl);
+    ngx_stat_handled  = (ngx_atomic_t *) (shared + 4 * cl);
+    ngx_stat_requests = (ngx_atomic_t *) (shared + 5 * cl);
+    ngx_stat_active   = (ngx_atomic_t *) (shared + 6 * cl);
+    ngx_stat_reading  = (ngx_atomic_t *) (shared + 7 * cl);
+    ngx_stat_writing  = (ngx_atomic_t *) (shared + 8 * cl);
+    ngx_stat_waiting  = (ngx_atomic_t *) (shared + 9 * cl);
+
+    #endif
+
+***************************************************
+* 赋值                                            *
+***************************************************
+
++--------------------+
+| ngx_event_accept() |
++--------------------+
+
+    accept()
+    (void) ngx_atomic_fetch_add(ngx_stat_accepted, 1);
+    ngx_get_connection()
+    (void) ngx_atomic_fetch_add(ngx_stat_active, 1);
+    创建基于请求的内存池，设置非阻塞，设置回调。
+    (void) ngx_atomic_fetch_add(ngx_stat_handled, 1);
+
++---------------------------+
+| ngx_http_create_request() |
++---------------------------+
+
+    (void) ngx_atomic_fetch_add(ngx_stat_reading, 1);
+    (void) ngx_atomic_fetch_add(ngx_stat_requests, 1);
+
++----------------------------+
+| ngx_http_process_request() |
++----------------------------+
+
+    (void) ngx_atomic_fetch_add(ngx_stat_reading, -1);
+    (void) ngx_atomic_fetch_add(ngx_stat_writing, 1);
+
++-------------------------+
+| ngx_http_free_request() |
++-------------------------+
+
+    (void) ngx_atomic_fetch_add(ngx_stat_reading, -1);
+    (void) ngx_atomic_fetch_add(ngx_stat_writing, -1);
+
++---------------------------+
+| ngx_reusable_connection() |
++---------------------------+
+
+    (void) ngx_atomic_fetch_add(ngx_stat_waiting, -1);
+    (void) ngx_atomic_fetch_add(ngx_stat_waiting, 1);
+
++---------------------------------+
+| ngx_close_accepted_connection() |
++---------------------------------+
+
+    (void) ngx_atomic_fetch_add(ngx_stat_active, -1);
+ */
 static ngx_int_t ngx_http_stub_status_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_stub_status_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
@@ -18,11 +111,12 @@ static char *ngx_http_set_stub_status(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 
 
+// 定义配置项
 static ngx_command_t  ngx_http_status_commands[] = {
 
     { ngx_string("stub_status"),
       NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS|NGX_CONF_TAKE1,
-      ngx_http_set_stub_status,
+      ngx_http_set_stub_status, // 配置项回调函数
       0,
       0,
       NULL },
@@ -31,6 +125,10 @@ static ngx_command_t  ngx_http_status_commands[] = {
 };
 
 
+/*
+ * 定义模块上下文
+ * preconfiguration() 阶段将当前模块自定义的变量加入全局存储结构
+ */
 static ngx_http_module_t  ngx_http_stub_status_module_ctx = {
     ngx_http_stub_status_add_variables,    /* preconfiguration */
     NULL,                                  /* postconfiguration */
@@ -46,6 +144,7 @@ static ngx_http_module_t  ngx_http_stub_status_module_ctx = {
 };
 
 
+// 定义 http 模块
 ngx_module_t  ngx_http_stub_status_module = {
     NGX_MODULE_V1,
     &ngx_http_stub_status_module_ctx,      /* module context */
@@ -62,6 +161,45 @@ ngx_module_t  ngx_http_stub_status_module = {
 };
 
 
+/*
+ * 定义变量数组
+ *
+ * <0>.此处未显式的设置 set_handler，实际上 set 操作分散到了 nginx 代码的很多地方。
+ *
+ *     (void) ngx_atomic_fetch_add(ngx_stat_reading, -1);
+ *     (void) ngx_atomic_fetch_add(ngx_stat_writing, 1);
+ *
+ * <1>.设置的 get_handler 均为 ngx_http_stub_status_variable。
+ *
+ *
+ * <2>.通常回调函数的 data 参数为存储变量值的地址。但是此处的 data 参数代表的是
+ *     变量的 id，在 get_handler 中通过判断 data 的值决定获取哪个参数。
+ *
+ *     Q:哪变量的值存储在哪里呢？
+ *     A:在 ngx_event_module_init() 函数中将全局指针指向共享内存的存储空间的操作就相当于 init 操作。
+ *
+ *       ngx_stat_accepted = (ngx_atomic_t *) (shared + 3 * cl);
+ *       ngx_stat_handled  = (ngx_atomic_t *) (shared + 4 * cl);
+ *       ngx_stat_requests = (ngx_atomic_t *) (shared + 5 * cl);
+ *       ngx_stat_active   = (ngx_atomic_t *) (shared + 6 * cl);
+ *       ngx_stat_reading  = (ngx_atomic_t *) (shared + 7 * cl);
+ *       ngx_stat_writing  = (ngx_atomic_t *) (shared + 8 * cl);
+ *       ngx_stat_waiting  = (ngx_atomic_t *) (shared + 9 * cl);
+ *       由此可见，变量的值是存储在共享内存中的。
+ *
+ *     Q:为何要存储到共享内存中呢？
+ *     A:因为 nginx 为多进程架构，多个 worker 进程同时运行时，只有把连接状态参数
+ *       都存储到共享内存中，该模块才能统计 nginx 整体的连接状态参数。
+ *
+ * <3>.变量的属性都是 NGX_HTTP_VAR_NOCACHEABLE 的。
+ *
+ *     NGX_HTTP_VAR_NOCACHEABLE 标示变量是频繁更新的，不可被缓存。并且该模块定
+ *     义的变量是要被多个 worker 进程共享，为了防止读写冲突，都是原子操作，更不
+ *     可能缓存变量的值了。
+ *
+ * 注意：在本模块中只是定义了变量，并未使用到变量。此模块定义变量，应该是为了让其他模块使用的而暴露出来的接口机制。
+ * 比如在定义日志格式时使用该模块中定义的变量。
+ */
 static ngx_http_variable_t  ngx_http_stub_status_vars[] = {
 
     { ngx_string("connections_active"), NULL, ngx_http_stub_status_variable,
@@ -80,6 +218,10 @@ static ngx_http_variable_t  ngx_http_stub_status_vars[] = {
 };
 
 
+/*
+ * brief  : 真正处理 stub_status 配置项的 handler
+ * param  : [in] r
+ */
 static ngx_int_t
 ngx_http_stub_status_handler(ngx_http_request_t *r)
 {
@@ -89,16 +231,19 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
     ngx_chain_t        out;
     ngx_atomic_int_t   ap, hn, ac, rq, rd, wr, wa;
 
+    // 仅支持 GET/HEAD 请求
     if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
         return NGX_HTTP_NOT_ALLOWED;
     }
 
+    // 丢弃请求头
     rc = ngx_http_discard_request_body(r);
 
     if (rc != NGX_OK) {
         return rc;
     }
 
+    // 构造响应头
     r->headers_out.content_type_len = sizeof("text/plain") - 1;
     ngx_str_set(&r->headers_out.content_type, "text/plain");
     r->headers_out.content_type_lowcase = NULL;
@@ -113,6 +258,7 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
         }
     }
 
+    // 计算响应数据的长度，分配存储空间
     size = sizeof("Active connections:  \n") + NGX_ATOMIC_T_LEN
            + sizeof("server accepts handled requests\n") - 1
            + 6 + 3 * NGX_ATOMIC_T_LEN
@@ -126,6 +272,7 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
     out.buf = b;
     out.next = NULL;
 
+    // 获取共享内存中的数据
     ap = *ngx_stat_accepted;
     hn = *ngx_stat_handled;
     ac = *ngx_stat_active;
@@ -134,6 +281,7 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
     wr = *ngx_stat_writing;
     wa = *ngx_stat_waiting;
 
+    // 拼接数据
     b->last = ngx_sprintf(b->last, "Active connections: %uA \n", ac);
 
     b->last = ngx_cpymem(b->last, "server accepts handled requests\n",
@@ -144,6 +292,7 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
     b->last = ngx_sprintf(b->last, "Reading: %uA Writing: %uA Waiting: %uA \n",
                           rd, wr, wa);
 
+    // 设置响应头
     r->headers_out.status = NGX_HTTP_OK;
     r->headers_out.content_length_n = b->last - b->pos;
 
@@ -160,6 +309,17 @@ ngx_http_stub_status_handler(ngx_http_request_t *r)
 }
 
 
+/*
+ * brief  : 变量的 get_handler() 回调函数。
+ * param  : [in] r : 指向请求的指针
+ * param  : [in/out] v : 将当前变量的值写入到 v 中。
+ * param  : [in] data : 定义变量时指定 set_handler/get_handler 的参数
+ * return : NGX_ERROR/NGX_OK
+ *
+ * 通常变量的值是存储在 data 中的，get_handler 通常的操作是将 data 中的数据拷贝到 v->data 中。
+ * 但是此模块有点特殊。此模块中变量的值是存储在共享内存中的，此处的 data 表示的是变量的 id，
+ * 根据不同的 id 就从共享内存中获取不同变量的值，然后再存储到 v->data 中。
+ */
 static ngx_int_t
 ngx_http_stub_status_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data)
@@ -196,7 +356,7 @@ ngx_http_stub_status_variable(ngx_http_request_t *r,
     }
 
     v->len = ngx_sprintf(p, "%uA", value) - p;
-    v->valid = 1;
+    v->valid = 1; // 设置为有效
     v->no_cacheable = 0;
     v->not_found = 0;
     v->data = p;
@@ -205,17 +365,23 @@ ngx_http_stub_status_variable(ngx_http_request_t *r,
 }
 
 
+/*
+ * brief  : 将自定义的变量加入全局存储结构中。
+ * return : NGX_OK/NGX_ERROR
+ */
 static ngx_int_t
 ngx_http_stub_status_add_variables(ngx_conf_t *cf)
 {
     ngx_http_variable_t  *var, *v;
 
     for (v = ngx_http_stub_status_vars; v->name.len; v++) {
+        // 返回的是当前变量的存储结构
         var = ngx_http_add_variable(cf, &v->name, v->flags);
         if (var == NULL) {
             return NGX_ERROR;
         }
 
+        // 注意：要设置 get_handler/set_handler/data
         var->get_handler = v->get_handler;
         var->data = v->data;
     }
@@ -224,6 +390,14 @@ ngx_http_stub_status_add_variables(ngx_conf_t *cf)
 }
 
 
+/* brief  : "stub_status" 配置项的回调。此模块是按需挂载，将当前模块注册到处理阶段中。
+ *
+ *           Q:什么时候使用按需挂载呢？
+ *           A:一般情况下，某个模块对某个 location 进行了处理以后，发现符合自己
+ *             处理的逻辑，而且也没有必要再调用 NGX_HTTP_CONTENT_PHASE 阶段的其
+ *             它 handler 进行处理的时候，就动态挂载上这个 handler。
+ * return : NGX_CONF_OK
+ */
 static char *
 ngx_http_set_stub_status(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
